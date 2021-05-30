9f790357fead117e23494fb9453c62844364ef90
wudizhangzhi <yueyatianchong@qq.com>
2021-05-30 21:15:45 +0800

    update

--- go_doc_table/document/base.go
+++ go_doc_table/document/base.go
@@ -32,17 +32,45 @@ type Col struct {
 
 // }
 
+func (c Col) String() string {
+	return strings.Join(c.Paragraphs, "")
+}
+
 func (t Table) String() string {
 	var s string
 	for _, row := range t.Rows {
 		cols_strings := []string{}
 		for _, col := range row.Cols {
-			col_str := strings.Join(col.Paragraphs, "")
+			col_str := col.String()
 			cols_strings = append(cols_strings, col_str)
 		}
 		s += fmt.Sprintf("| %s |\n", strings.Join(cols_strings, " | "))
 	}
 	return s
+}
+
+// table转化为map
+func (t Table) ToMap() map[string]string {
+	m := make(map[string]string)
+	for _, row := range t.Rows {
+		colNum := len(row.Cols)
+		switch {
+		case colNum == 1:
+			// 跳过
+			continue
+		case colNum == 2:
+			// 第一个是key， 第二个是value
+			m[row.Cols[0].String()] = row.Cols[1].String()
+		case colNum == 3:
+			//
+		case colNum == 4:
+			// 2对
+			m[row.Cols[0].String()] = row.Cols[1].String()
+			m[row.Cols[2].String()] = row.Cols[3].String()
+		}
+
+	}
+	return m
 }
 
 // 占位数量
